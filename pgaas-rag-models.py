7ee88178cb2909e0772c46867b0a23b0b696d0ee
Rob Annable <81085060+robannable@users.noreply.github.com>
2024-10-06 19:19:00 +0100

    Update pgaas-rag-models.py

--- pgaas-rag-models.py
+++ pgaas-rag-models.py
@@ -23,12 +23,8 @@ except ImportError:
 # Available models
 AVAILABLE_MODELS = [
     "mistral-7b-instruct",
-    "mistral-8x7b-instruct",
-    "llama-3.1-sonar-small-128k-online",
-    "llama-3.1-sonar-small-128k-chat",
     "llama-3.1-sonar-large-128k-online",
     "llama-3.1-sonar-large-128k-chat",
-    "llama-3.1-8b-instruct",
     "llama-3.1-70b-instruct"
 ]
 
